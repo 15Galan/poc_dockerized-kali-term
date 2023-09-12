d1a2782f68f01f8491bb30860b545e89cd838bd5
Sr. Galán <antoniogalanG15@gmail.com>
2023-09-12 22:06:08 +0200

    fix: tabulación mal formada

--- debug.sh
+++ debug.sh
@@ -14,9 +14,9 @@ docker build . -t kali-dotfiles
 # Crear un contenedor
 docker run \
 	-itd \
-        --name dotkali \
-        -p 5555:5555 \
-        -h ${HOSTNAME}-kali \
-        -e DISPLAY \
-        -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-        kali-dotfiles
+    --name dotkali \
+    -p 5555:5555 \
+    -h ${HOSTNAME}-kali \
+    -e DISPLAY \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    kali-dotfiles
